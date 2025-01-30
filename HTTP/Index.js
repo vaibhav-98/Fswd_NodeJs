0c9fb2edf5555bc4044ad76e3a1a694799e38ac2
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2025-01-30 20:05:59 +0530

    class push

---
+++ HTTP/Index.js
@@ -0,0 +1,14 @@
+const http = require("http")
+
+const Server = http.createServer( (req,res) => {
+     if(req.url == '/') {
+        res.write('<h1> Hello , Node js server <h1>')
+     }
+     res.end();
+})
+
+Server.listen(3000);
+
+console.log("THe Http Server is running port no, 5000");
+
+
---
+++ HTTP/index2.js
@@ -0,0 +1,28 @@
+const http = require("http")      
+
+const PORT = 3004
+const HOSTNAME = "localhost"
+
+ const server = http.createServer( (req,res) => {
+    //Home Page
+    if(req.url == "/") {
+        res.statusCode = 200;
+        res.end("Welcome to node js Server Created by Amiy")
+    }
+
+    //About Page
+    if(req.url == "/about") {
+        res.statusCode = 200;
+        res.end("<h1>Welcome to node js Server Created by Prakash<h1>")
+    }
+ })
+
+ server.listen(PORT, () => {
+    console.log(`Server running at${HOSTNAME} :${PORT}`);
+    
+ })
+
+
+
+
+
